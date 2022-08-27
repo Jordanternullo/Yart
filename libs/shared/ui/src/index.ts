784b825f13339e2821bf4e3e1e3f0ea51646f4e3
Jordanternullo <jordan.ternullo@gmail.com>
2022-08-27 09:58:49 +0200

    feat(project): create file input component (design and storybook)

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -3,6 +3,7 @@ export * from './lib/components/nav/nav-item/nav-item';
 export * from './lib/components/avatar/avatar';
 export * from './lib/components/card/card';
 export * from './lib/components/forms/checkbox/checkbox';
+export * from './lib/components/forms/file/file';
 export * from './lib/components/forms/radio/radio';
 export * from './lib/components/forms/input/input';
 export * from './lib/components/forms/toggle/toggle';
